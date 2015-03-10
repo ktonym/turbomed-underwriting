8f0b07aff9de2eb07b2aaaf50d415b7b091af4cf
ktonym <ktonym@gmail.com>
2015-03-10 17:58:51 +0300

    Enhanced the UserController.js but without the methods

--- src/main/webapp/app/controller/UserController.js
+++ src/main/webapp/app/controller/UserController.js
@@ -1,3 +1,26 @@
 Ext.define('EMIS.controller.UserController', {
-    extend: 'Ext.app.Controller'
+    extend: 'Ext.app.Controller',
+    views: ['user.ManageUsers'],
+    refs: [{
+        ref: 'userForm',
+        selector: 'manageusers userform'
+    },{
+        ref: 'userList',
+        selector: 'manageusers userlist'
+    },{
+        ref: 'addUserButton',
+        selector: 'manageusers button #addUserBtn'
+    },{
+        ref: 'saveUserButton',
+        selector: 'manageusers userform button #saveBtn'
+    },{
+        ref: 'deleteUserButton',
+        selector: 'manageusers userform button #deleteBtn'
+    },{
+        ref: 'userFormFieldset',
+        selector: 'manageusers userform fieldset'
+    },{
+        ref: 'usernameField',
+        selector: 'manageusers userform textfield[name=username]'
+    }]
 });
