62e715187701f131c648597d2a335503200c1641
Anthony Kip <ktonym@gmail.com>
2015-03-11 20:35:11 +0300

    Enhanced the UserController.js: completed the doSaveUser function

--- src/main/webapp/app/controller/UserController.js
+++ src/main/webapp/app/controller/UserController.js
@@ -53,5 +53,57 @@ Ext.define('EMIS.controller.UserController', {
         var newUserRec = Ext.create('EMIS.model.User');
         me.getUserForm().loadRecord(newUserRec);
         me.getDeleteUserButton().disable();
+    },
+    doDeleteUser: function(){
+        var me = this;
+        var rec = me.getUserForm().getRecord();
+        Ext.Msg.confirm('Confirm Delete User','Are you sure you want to delete user ' +
+        rec.get('firstName')+'?',function
+            (btn){
+            if(btn ==='yes'){
+                rec.destroy({
+                    success: function(rec,operation){
+                        Ext.Msg.alert('Delete success',operation.request.scope.reader.jsonData.msg);
+                    },
+                    failure: function(rec,operation){
+                        Ext.Msg.alert('Delete Failure', operation.request.scope.reader.jsonData.msg);
+                    }
+                });
+            }
+        });
+    },
+    doSelectUser: function(grid,record){
+        var me = this;
+        me.getUserForm().loadRecord(record);
+        me.getUserFormFieldset().setTitle('Edit User ' + record.data.username);
+        me.getUsernameField().disable();
+        me.getDeleteUserButton().enable();
+    },
+    doSaveUser: function(){
+        var me = this;
+        var rec = me.getUserForm().getRecord();
+        if(rec !== null){
+            me.getUserForm().updateRecord();
+            var errs = rec.validate();
+            if (errs.isValid()){
+                rec.save({
+                    success: function(record, operation){
+                        if(typeof record.store === 'undefined'){
+                            me.getUserList().getStore().add(record);
+                            me.getUserList().getSelectionModel().select(record,true);
+                        }
+                        me.getUserFormFieldset().setTitle('Edit User ' + record.data.username);
+                        me.getUsernameField().disable();
+                        me.getDeleteUserButton().enable();
+                    },
+                    failure: function(rec,operation){
+                        Ext.Msg.alert('Save Failure', operation.request.scope.reader.jsonData.msg);
+                    }
+                });
+            } else {
+                me.getUserForm().getForm().markInvalid(errs);
+                Ext.Msg.lert('Invalid Fields','Please fix the invalid entries');
+            }
+        }
     }
 });
