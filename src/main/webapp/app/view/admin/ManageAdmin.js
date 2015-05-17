c2f4a8e88fcb72ab09b47a911c791b7158fdd037
ktonym <ktonym@gmail.com>
2015-05-17 23:38:37 +0300

    Modified view: ManageAdmin, defined layout and items

--- src/main/webapp/app/view/admin/ManageAdmin.js
+++ src/main/webapp/app/view/admin/ManageAdmin.js
@@ -1,4 +1,53 @@
 Ext.define("EMIS.view.admin.ManageAdmin", {
-    extend: 'Ext.Component',
-    html: 'Hello, World!!'
+
+    extend: 'Ext.panel.Panel',
+
+    xtype: 'manageadmin',
+
+    requires: [],
+
+    layout: {
+        type: 'hbox',
+        align: 'stretch'
+    },
+
+    initComponent: function(){
+        var me = this;
+        Ext.applyIf(me,{
+            items: [
+
+                {
+                    xtype: '',
+                    width: 100
+                },
+                {
+                    xtype: 'container',
+                    itemId: 'manageAdminCards',
+                    activeItem: 0,
+                    flex: 1,
+                    layout: {
+                        type: 'card'
+                    },
+                    items: [
+
+                        {
+                            xtype: 'container',
+                            padding: 10,
+                            html: 'Please select an item from the menu'
+                        },{
+                            xtype: 'accountmapping'
+                        },{
+                            xtype: 'premiumrates'
+                        },{
+                            xtype: 'intermediaries'
+                        }
+
+                    ]
+                }
+
+            ]
+        });
+        me.callParent(arguments);
+    }
+
 });
